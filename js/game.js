a35d8b262484d2bd4134cc30a35e2d7e4bb5aacc
Melvin Snijders <mail@melvinsnijders.nl>
2022-02-10 14:33:20 +0100

    Level 2-5

--- js/game.js
+++ js/game.js
@@ -16,3 +16,18 @@ const Game = (function (url) {
     }
 
 })(apiUrl);
+
+Game.Reversi = (function(){
+    let configMap = {
+
+    }
+
+    const privateInit = function () {
+        console.log("Init vanuit Reversi!")
+    }
+
+    return {
+        init: privateInit
+    }
+
+})()
