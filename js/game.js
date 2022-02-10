aec173cee98ed551bf8383ba1e57e001b0c7f05a
Melvin Snijders <mail@melvinsnijders.nl>
2022-02-10 13:56:02 +0100

    Level 2-2

--- js/game.js
+++ js/game.js
@@ -1,3 +1,14 @@
 const Game = (function () {
     console.log("hello from module");
+
+    // Private function init
+    const privateInit = function(){
+        console.log('Private information!');
+    }
+
+    // Waarde/object geretourneerd aan de outer scope
+    return {
+        init: privateInit
+    }
+
 })();
